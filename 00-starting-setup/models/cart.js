9bea6807539010d4980d9add5967e0e28be93e71
Bharat Bhandari <bharatbhandari0302@gmail.com>
2023-12-29 23:45:23 +0530

    add mongodb

---
+++ 00-starting-setup/models/cart.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  static addProduct(id) {
+    // fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // Analyze the cart => find existing product
+      
+      
+
+    });
+    // Add new product / increase quantity
+  }
+};
